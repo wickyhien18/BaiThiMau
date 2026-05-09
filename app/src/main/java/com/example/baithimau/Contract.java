23ab8c05cb88215bd0170ad0b3dc7e2456632ff8
wickyhien18 <giaphien1008@gmail.com>
2026-05-09 15:01:26 +0700

    create Contract

---
+++ app/src/main/java/com/example/baithimau/Contract.java
@@ -0,0 +1,41 @@
+package com.example.baithimau;
+
+public class Contract {
+
+    private Integer Id;
+    private String Name;
+    private String PhoneNumber;
+
+    public Contract() {
+    }
+
+    public Contract(Integer id, String name, String phoneNumber) {
+        Id = id;
+        Name = name;
+        PhoneNumber = phoneNumber;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+}
--- app/src/main/java/com/example/baithimau/MainActivity.java
+++ app/src/main/java/com/example/baithimau/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.baithimau;
 
 import android.os.Bundle;
+import android.widget.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button add;
+    private ListView list;
+    private EditText search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        Init();
+    }
+
+    private void Init() {
 
     }
 }
