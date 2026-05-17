db4748aaf585475ab162b1a9a2ff9ccb5b32798a
wickyhien18 <giaphien1008@gmail.com>
2026-05-17 10:46:09 +0700

    add Adapter.java

---
+++ app/src/main/java/com/example/baithimau/Adapter.java
@@ -0,0 +1,24 @@
+package com.example.baithimau;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class Adapter extends ArrayAdapter<Contract> {
+    public Adapter(@NonNull Context context, int resource, @NonNull ArrayList<Contract> objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.item, parent, false);
+    }
+}
